8f8fbfb56f3a0d029222411aebb206f0f4d691ea
akdevlabs <116324447+akdevlabs@users.noreply.github.com>
2023-07-21 00:53:42 -0600

    Add files via upload

---
+++ Recursos.js
@@ -0,0 +1,289 @@
+import {recurso1} from './baseDatosRec.js'
+import {recurso2} from './baseDatosRec.js'
+import {recurso3} from './baseDatosRec.js'
+
+//------------------ Render ----------------------//
+let rOne = recurso1
+let rTwo = recurso2
+let rThree = recurso3
+
+
+
+let courseLevel = 1
+
+
+function selectCourse() {
+ 
+  if (courseLevel === 0){
+    let blockOne = rOne 
+    return blockOne
+ 
+  }else if (courseLevel === 1){
+    let blockTwo = rTwo
+    return blockTwo
+
+  }else{
+    let blockThree = rThree 
+    return blockThree}
+}
+
+function one(){
+   let course =  selectCourse()
+   return course.imgOne 
+}
+
+function oneWeb(){
+  let course =  selectCourse()
+  return course.weblinkOne
+}
+
+function two(){
+    let course =  selectCourse()
+    return course.imgTwo 
+}
+
+function twoWeb(){
+  let course =  selectCourse()
+  return course.weblinkTwo
+}
+
+function three(){
+  let course =  selectCourse()
+  return course.imgThree
+}
+
+function threeWeb(){
+  let course = selectCourse()
+  return (course.weblinkThree)
+}
+
+function Four(){
+  let course =  selectCourse()
+  return course.imgFour
+}
+
+function fourWeb(){
+  let course = selectCourse()
+  return (course.weblinkFour)
+}
+
+function five(){
+  let course =  selectCourse()
+  return course.imgFive
+}
+
+
+function fiveWeb(){
+  let course = selectCourse()
+  return (course.weblinkFive)
+}
+
+function six(){
+  let course =  selectCourse()
+  return course.imgSix
+}
+
+
+function sixWeb(){
+  let course = selectCourse()
+  return (course.weblinkSix)
+}
+
+function seven(){
+  let course =  selectCourse()
+  return course.imgSeven
+}
+
+
+function sevenWeb(){
+  let course = selectCourse()
+  return (course.weblinkSeven)
+}
+
+function eight(){
+  let course =  selectCourse()
+  return course.imgEight
+}
+
+
+function eightWeb(){
+  let course = selectCourse()
+  return (course.weblinkEight)
+}
+
+function nine(){
+  let course =  selectCourse()
+  return course.imgNine
+}
+
+
+function nineWeb(){
+  let course = selectCourse()
+  return (course.weblinkNine)
+}
+
+function ten(){
+  let course =  selectCourse()
+  return course.imgTen
+}
+
+
+function tenWeb(){
+  let course = selectCourse()
+  return (course.weblinkTen)
+}
+
+
+
+
+
+
+function cWebone(){
+const link = document.getElementById("link")
+  link.href = oneWeb() 
+  return(link.href)
+}
+cWebone()
+
+function cWebtwo(){
+const link = document.getElementById("link1")
+  link.href = twoWeb()
+  return(link.href)
+}
+cWebtwo()
+ 
+function cWebthree(){
+  const link = document.getElementById("link2")
+  link.href = threeWeb()
+  return(link.href)
+}
+cWebthree()
+  
+function cWebfour(){
+  const link = document.getElementById("link3")
+  link.href = fourWeb()
+  return(link.href)
+}
+cWebfour()
+
+function cWebfive(){
+  const link = document.getElementById("link4")
+  link.href = fiveWeb()
+  return(link.href)
+}
+cWebfive()
+
+function cWebsix(){
+  const link = document.getElementById("link5")
+  link.href = sixWeb()
+  return(link.href)
+}
+cWebsix()
+
+function cWebseven(){
+  const link = document.getElementById("link6")
+  link.href = sevenWeb()
+  return(link.href)
+}
+cWebseven()
+
+function cWebeight(){
+  const link = document.getElementById("link7")
+  link.href = eightWeb()
+  return(link.href)
+}
+cWebeight()
+
+function cWebnine(){
+  const link = document.getElementById("link8")
+  link.href = nineWeb()
+  return(link.href)
+}
+cWebnine()
+
+function cWebten(){
+  const link = document.getElementById("link9")
+  link.href = tenWeb()
+  return(link.href)
+}
+cWebten()
+
+function oneC(){
+const iin = document.getElementById("show")
+iin.src = one()
+let want = iin.src
+return(want)
+}
+oneC()
+
+function twoC(){
+const iin = document.getElementById("show2")
+iin.src = two()
+let want = iin.src
+return(want)
+}
+twoC()
+
+function threeC(){
+const iin = document.getElementById("show3")
+iin.src = three()
+let want = iin.src
+return(want)
+}    
+threeC()
+
+function fourC(){
+const iin = document.getElementById("show4")
+iin.src = Four()
+let want = iin.src
+return(want)
+}    
+fourC()
+
+function fiveC(){
+const iin = document.getElementById("show5")
+iin.src = five()
+let want = iin.src
+return(want)
+}    
+fiveC()
+
+function sixC(){
+const iin = document.getElementById("show6")
+iin.src = six()
+let want = iin.src
+return(want)
+}    
+sixC()
+
+function sevenC(){
+const iin = document.getElementById("show7")
+iin.src = seven()
+let want = iin.src
+return(want)
+} 
+sevenC()
+
+function eightC(){
+const iin = document.getElementById("show8")
+iin.src = eight()
+let want = iin.src
+return(want)
+}    
+eightC()
+
+function nineC(){
+const iin = document.getElementById("show9")
+iin.src = nine()
+let want = iin.src
+return(want)
+}    
+nineC()
+
+function tenC(){
+const iin = document.getElementById("show10")
+iin.src = ten()
+let want = iin.src
+return(want)
+}    
+tenC()
---
+++ baseDatosRec.js
@@ -0,0 +1,100 @@
+export const recurso1 = {
+  imgOne:"A1-A2.png",
+  weblinkOne: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1r056-which-animal" ,
+
+  imgTwo:"A1-A21.png",
+  weblinkTwo: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a2w003-giving-advice-to-a-friend" ,
+
+  imgThree:"A1-A22.png",
+  weblinkThree: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1r002-the-teacher" ,
+
+  imgFour:"A1-A23.png",
+  weblinkFour: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1w003-what-you-did-last-saturday" ,
+
+  imgFive:"A1-A24.png",
+  weblinkFive: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1w001-an-email-to-a-friend" ,
+
+  imgSix:"A1-A25.png",
+  weblinkSix: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a2w002-a-holiday-cottage-for-rent" ,
+
+  imgSeven:"A1-A26.png",
+  weblinkSeven: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1w004-filling-in-a-form" ,
+
+  imgEight:"A1-A27.png",
+  weblinkEight: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1g001-how-to-write-plurals" ,
+
+  imgNine:"A1-A28.png",
+  weblinkNine: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1g003-past-simple-of-irregular-verbs" ,
+
+  imgTen:"A1-A29.png",
+  weblinkTen:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/a1g002-present-simple-and-present-continuous-questions",
+
+
+  
+ }
+
+export const recurso2 = {
+ imgOne:"B1-B2.png",
+ weblinkOne: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1g055-family-holiday" ,
+
+ imgTwo:"B1-B21.png", 
+ weblinkTwo:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1g057-what-a-great-idea",
+
+ imgThree:"B1-B22.png", 
+ weblinkThree:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1g059-see-and-know",
+
+ imgFour:"B1-B23.png", 
+ weblinkFour:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1l054-listening-to-dialogues",
+
+ imgFive:"B1-B24.png", 
+ weblinkFive:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1g060-actually-and-actual",
+
+ imgSix:"B1-B25.png", 
+ weblinkSix:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1g087-very-really-or-absolutely",
+
+ imgSeven:"B1-B26.png", 
+ weblinkSeven:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1l080-tastes",
+
+ imgEight:"B1-B27.png", 
+ weblinkEight:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1l055-how-do-you-feel",
+
+ imgNine:"B1-B28.png", 
+ weblinkNine:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1l082-amir-s-adventure",
+
+ imgTen:"B1-B29.png", 
+ weblinkTen:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/b1p068-word-stress",
+
+}
+
+export const recurso3 = {
+  imgOne:"C1-C2.png",
+  weblinkOne: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1p065-intonation",
+
+  imgTwo:"C1-C21.png",
+  weblinkTwo: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1g059-fronting-substitution-and-ellipsis",
+
+  imgThree:"C1-C22.png",
+  weblinkThree: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1s002-stressed-words-and-meaning",
+
+  imgFour:"C1-C23.png",
+  weblinkFour: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1p054-word-stress",
+
+  imgFive:"C1-C24.png",
+  weblinkFive: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1p064-us-or-uk-pronunciation",
+
+  imgSix:"C1-C25.png",
+  weblinkSix: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c2p055-word-stress-four-syllable-words",
+
+  imgSeven:"C1-C26.png",
+  weblinkSeven: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1g002-which-as-a-relative-pronoun",
+
+  imgEight:"C1-C27.png",
+  weblinkEight: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1g001-reporting-verbs" ,
+  
+  imgNine:"C1-C28.png",
+  weblinkNine: "https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1g060-ellipsis" ,
+
+  imgTen:"C1-C29.png",
+  weblinkTen:"https://www.cambridgeenglish.org/learning-english/activities-for-learners/c1w074-cover-letter",
+
+ }
